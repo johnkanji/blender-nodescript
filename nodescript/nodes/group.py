e0b1350f44d6538a7b5095601f5a76f7d87e0e82
John Kanji <jkanji@pm.me>
2021-03-20 16:41:08 -0400

    func to node group

--- nodescript/nodes/group.py
+++ nodescript/nodes/group.py
@@ -4,6 +4,7 @@ from ..type_system import *
 class _GroupInput(NodeBase):
     def __init__(self, inps):
         self._inputs = inps
+        self._translation = {}
         super(_GroupInput, self).__init__()
     
     @property
@@ -18,15 +19,20 @@ class _GroupInput(NodeBase):
     def outputs(self):
         return self._inputs
     
+    @property
+    def translation(self):
+        return self._translation
+    
 class _GroupOutput(NodeBase):
     def __init__(self, outs, params):
         self._outputs = outs
+        self._translation = {}
         print('in _GroupOutput constr', params)
         super(_GroupOutput, self).__init__(params)
     
     @property
     def bnode(self):
-        return 'NodeGroupInput'
+        return 'NodeGroupOutput'
     
     @property
     def inputs(self):
@@ -35,3 +41,7 @@ class _GroupOutput(NodeBase):
     @property
     def outputs(self):
         return {}
+    
+    @property
+    def translation(self):
+        return self._translation
--- nodescript/operators.py
+++ nodescript/operators.py
@@ -1,6 +1,8 @@
 import bpy
 from . import parse
 
+from nodescript.type_system import BType, GraphMode, Tree
+
 
 class NodescriptCompile(bpy.types.Operator):
     """Compile Nodescript into Blender NodeTree"""
@@ -8,29 +10,67 @@ class NodescriptCompile(bpy.types.Operator):
     bl_label = "Compile"
     bl_options = {'UNDO'}
 
+    MODE_TO_TYPE = {
+        GraphMode.SHADER: 'ShaderNodeTree',
+        GraphMode.COMPOSITING: 'CompositorNodeTree'
+    }
+
+    BTYPE_TO_INOUT = {
+        BType.VALUE: 'NodeSocketFloat',
+        BType.VECTOR: 'NodeSocketVector',
+        BType.COLOR: 'NodeSocketColor',
+        BType.SHADER: 'NodeSocketShader',
+    }
+
     def execute(self, context):
         for tree in parse.parse(bpy.context.edit_text.as_string().strip()):
             print('PARSE DONE')
             print('Graph type:', tree.mode, 'func' if tree.is_func else '')
             print('Tree Name:', tree.name)
-            for k, n in tree.nodes.items():
-                print(n, k)
-                for a, p in n.params.items():
-                    print(' ', a, p)
+            # for k, n in tree.nodes.items():
+            #     print(n, k)
+            #     for a, p in n.params.items():
+            #         print(' ', a, p)
+            print()
 
-        # mat_name = parser.name
-        # mat = (bpy.data.materials.get(mat_name) or
-        #        bpy.data.materials.new(mat_name))
-        # mat.use_nodes = True
+        btree = self.setup_tree(tree)
+        if tree.is_func:
+            self.setup_group_inout(tree, btree)
 
-        # tree = mat.node_tree
-        # for node in tree.nodes:
-        #     tree.nodes.remove(node)
-
-        # id_to_bnode = self.to_bnodes(parser.nodes, tree)
-        # self.layout_nodes(parser.nodes, id_to_bnode)
+        id_to_bnode = self.to_bnodes(tree.nodes, btree)
+        self.layout_nodes(tree.nodes, id_to_bnode)
 
         return {'FINISHED'}
+
+    def setup_tree(self, tree: Tree):
+        if tree.is_func:
+            btree = (bpy.data.node_groups.get(tree.name) or
+                    bpy.data.node_groups.new(tree.name, self.MODE_TO_TYPE[tree.mode]))
+        else:
+            if tree.mode == GraphMode.SHADER:
+                obj = (bpy.data.materials.get(tree.name) or
+                       bpy.data.materials.new(tree.name))
+            elif tree.mode == GraphMode.COMPOSITING:
+                obj = bpy.context.scene
+            obj.use_nodes = True
+            btree = obj.node_tree
+
+        for node in btree.nodes:
+            btree.nodes.remove(node)
+        return btree
+
+    def setup_group_inout(self, tree, btree):
+        inp = next(filter(lambda n: n.bnode == 'NodeGroupInput', tree.nodes.values()))
+        btree.inputs.clear()
+        for p, t in inp._inputs.items():
+            sock = btree.inputs.new(self.BTYPE_TO_INOUT[t], p)
+            inp._translation[p] = sock.identifier.lower()
+
+        out = next(filter(lambda n: n.bnode == 'NodeGroupOutput', tree.nodes.values()))
+        btree.outputs.clear()
+        for p, t in out._outputs.items():
+            sock = btree.outputs.new(self.BTYPE_TO_INOUT[t], p)
+            out._translation[p] = sock.identifier.lower()
 
     def to_bnodes(self, nodes, tree):
         bnodes = tree.nodes
@@ -58,11 +98,6 @@ class NodescriptCompile(bpy.types.Operator):
 
         return id_to_bnode
 
-    def get_node_socket(self, sockets, name):
-        for sock in sockets:
-            if sock.identifier.lower() == name:
-                return sock
-
     def layout_nodes(self, nodes, bnodes):
         HSPACE = 75
         VSPACE = 30
@@ -87,3 +122,8 @@ class NodescriptCompile(bpy.types.Operator):
                     prev = bnodes[layer_nodes[j-1].id]
                     prev_bottom = prev.location[1] - prev.dimensions[1]/2
                     b.location = (xpos, prev_bottom - VSPACE)
+
+    def get_node_socket(self, sockets, name):
+        for sock in sockets:
+            if sock.identifier.lower() == name:
+                return sock
